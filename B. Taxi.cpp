3fda0cf6171f4916986663d4faaf8139425bd5c3
Salah Elshafey <112121144+SalahElshafey@users.noreply.github.com>
2022-08-26 00:09:07 +0200

    Update B. Taxi.cpp

--- B. Taxi.cpp
+++ B. Taxi.cpp
@@ -1,37 +1,45 @@
-#include<bits/stdc++.h>
-using namespace std;
-int total=0;
+#include<stdio.h>
+#include<string.h>
 int main()
 {
-        int n,s,count[5]={0,0,0,0,0};
-        cin>>n;
-        for(int i=0;i<n;i++)
-        {
-            cin>>s;
-            count[s]++;
-      }
- 
-      total+=count[4]+count[3];
-      if(count[1]>count[3])
-      {
-            if(count[2]%2!=0)
-            {
-                  int a=count[1],b=count[3];
-                  a-=b;
-                  int c=(a+count[2]*2+3)/4;
-                  total+=c;
-            }
-            else
-            {
-                 int a=count[1],b=count[3];
-                  a-=b;
-                total+=count[2]/2+(a+3)/4;
-            }
-      }
-      else
-      {
-            total+=(count[2]+1)/2;
-      }
-      cout<<total;
-      return 0;
+	
+	int a1,a2,a3;
+	int i,n,c,l;
+	while(scanf("%d",&n)!=EOF)
+	{
+		i=0;
+		c=0;a1=0,a2=0,a3=0;
+		while(i<n)
+		{
+			scanf("%d",&l);
+			if(l==4) c++;
+			else if(l==1) a1++;
+			else if(l==2) a2++;
+			else if(l==3) a3++;
+			i++;
+		}
+		if(a3<a1)
+		{
+			c+=a3;
+			a1-=a3;
+			a3=0;
+		}
+		else
+		{
+			c+=a3;
+			a1=0;a3=0;
+		}
+		c+=a2/2;
+		if(a2%2)
+		{
+			c++;a1-=2;
+		}
+		if(a1>0)
+		{
+			c+=a1/4;
+			if(a1%4) c++;
+		}
+		printf("%d\n",c);
+	}
+	return 0;
 }
